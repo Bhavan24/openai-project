9062db61e03d119742bbe3a791bc37400514c3b1
bhavan <bhavanl@techprovint.com>
2023-02-03 23:59:08 +0530

    Format code

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyD_junLbnfNALmFGxBaA40TwFloKYYrWZQ',
+    authDomain: 'charming-bolt-319705.firebaseapp.com',
+    projectId: 'charming-bolt-319705',
+    storageBucket: 'charming-bolt-319705.appspot.com',
+    messagingSenderId: '642913770833',
+    appId: '1:642913770833:web:96bb83efadbb5af856c770',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
