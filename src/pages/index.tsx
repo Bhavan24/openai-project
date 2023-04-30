4be48c0e33ef74fe3a605883c151cf5c628c45dd
bhavan <bhavanl@techprovint.com>
2023-04-30 18:18:44 +0530

    Condition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
-import { Typography } from '@material-tailwind/react';
-import Link from 'next/link';
-import React from 'react';
+import AdvancedPage from './advanced';
+import BasicPage from './basic';
 
 export default function Dashboard() {
-    return (
-        <>
-            <div className="flex flex-col m-4">
-                <a href="/advanced">Advanced Page</a>
-                <a href="/basic">Basic Page</a>
-            </div>
-        </>
-    );
+    const displayBasic = false;
+
+    return displayBasic ? <BasicPage /> : <AdvancedPage />;
 }
