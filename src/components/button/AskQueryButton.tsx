a6a22332c2a9e1ef0261be4a7d35c9cf868365f3
bhavan <bhavanl@techprovint.com>
2023-05-07 01:50:50 +0530

    Image

--- src/components/button/AskQueryButton.tsx
+++ src/components/button/AskQueryButton.tsx
@@ -1,5 +1,7 @@
 import { OpenAIService } from '@/config';
-import React, { useCallback, useState } from 'react';
+import { GPT_API_TYPES } from '@/constants';
+import { SettingsContext } from '@/context';
+import React, { useCallback, useContext, useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
 import { CustomButton } from '../custom';
 
@@ -11,105 +13,131 @@ interface AskQueryButtonProps {
 
 const AskQueryButton: React.FC<AskQueryButtonProps> = ({ model, text, onComplete }) => {
     const [submitting, setSubmitting] = useState<boolean>(false);
+    const { settings } = useContext(SettingsContext);
 
     const searchGpt = useCallback(() => {
         const openAiTest = async () => {
-            const { data } = await OpenAIService.createCompletion({
-                model: model,
-                prompt: text.trim(),
-                max_tokens: 2048,
-                n: 1,
-                stop: '',
-                temperature: 0.5,
-            });
+            switch (settings.type) {
+                case GPT_API_TYPES[0].value: {
+                    const { data } = await OpenAIService.createCompletion({
+                        model: model,
+                        prompt: text.trim(),
+                        max_tokens: 2048,
+                        n: 1,
+                        stop: '',
+                        temperature: 0.5,
+                    });
 
-            // await OpenAIService.createChatCompletion({
-            //     model: '',
-            //     messages: []
-            // });
-
-            // await OpenAIService.createEdit({
-            //     model: '',
-            //     instruction: ''
-            // });
-
-            // await OpenAIService.createEmbedding({
-            //     model: '',
-            //     input: ''
-            // });
-
-            // await OpenAIService.createFile({});
-
-            // await OpenAIService.createImage({
-            //     prompt: ''
-            // });
-
-            // await OpenAIService.createFineTune({
-            //     training_file: ''
-            // });
-
-            // await OpenAIService.createImageEdit();
-
-            // await OpenAIService.createImageVariation({
-            //     lastModified: 0,
-            //     name: '',
-            //     webkitRelativePath: '',
-            //     size: 0,
-            //     type: '',
-            //     arrayBuffer: function (): Promise<ArrayBuffer> {
-            //         throw new Error('Function not implemented.');
-            //     },
-            //     slice: function (start?: number | undefined, end?: number | undefined, contentType?: string | undefined): Blob {
-            //         throw new Error('Function not implemented.');
-            //     },
-            //     stream: function (): ReadableStream<Uint8Array> {
-            //         throw new Error('Function not implemented.');
-            //     },
-            //     text: function (): Promise<string> {
-            //         throw new Error('Function not implemented.');
-            //     },
-            //     prototype: undefined
-            // });
-
-            // await OpenAIService.createModeration({
-            //     input: ''
-            // });
-
-            // await OpenAIService.createTranscription({
-
-            // });
-
-            // await OpenAIService.createTranslation();
-
-            // await OpenAIService.deleteFile();
-
-            // await OpenAIService.deleteModel();
-
-            // await OpenAIService.downloadFile();
-
-            // await OpenAIService.listFiles();
-
-            // await OpenAIService.listFineTuneEvents();
-
-            // await OpenAIService.listFineTunes();
-
-            // await OpenAIService.listModels();
-
-            // await OpenAIService.retrieveFile();
-
-            // await OpenAIService.retrieveFineTune();
-
-            // await OpenAIService.retrieveModel();
-
-            const response = data.choices[0].text?.trim() || '';
-            onComplete(response);
+                    const response = data.choices[0].text?.trim() || '';
+                    onComplete(response);
+                }
+                case GPT_API_TYPES[1].value: {
+                    await OpenAIService.createChatCompletion({
+                        model: model, // 'gpt-3.5-turbo'
+                        messages: [{ role: 'user', content: text.trim() }],
+                    });
+                }
+                case GPT_API_TYPES[2].value: {
+                    await OpenAIService.createEdit({
+                        model: '',
+                        instruction: '',
+                    });
+                }
+                case GPT_API_TYPES[3].value: {
+                    await OpenAIService.createEmbedding({
+                        model: '',
+                        input: '',
+                    });
+                }
+                case GPT_API_TYPES[4].value: {
+                    // await OpenAIService.createFile({});
+                }
+                case GPT_API_TYPES[5].value: {
+                    await OpenAIService.createImage({
+                        prompt: text.trim(),
+                    });
+                }
+                case GPT_API_TYPES[6].value: {
+                    // await OpenAIService.createFineTune({
+                    //     training_file: ''
+                    // });
+                }
+                case GPT_API_TYPES[7].value: {
+                    // await OpenAIService.createImageEdit();
+                }
+                case GPT_API_TYPES[8].value: {
+                    // await OpenAIService.createImageVariation({
+                    //     lastModified: 0,
+                    //     name: '',
+                    //     webkitRelativePath: '',
+                    //     size: 0,
+                    //     type: '',
+                    //     arrayBuffer: function (): Promise<ArrayBuffer> {
+                    //         throw new Error('Function not implemented.');
+                    //     },
+                    //     slice: function (start?: number | undefined, end?: number | undefined, contentType?: string | undefined): Blob {
+                    //         throw new Error('Function not implemented.');
+                    //     },
+                    //     stream: function (): ReadableStream<Uint8Array> {
+                    //         throw new Error('Function not implemented.');
+                    //     },
+                    //     text: function (): Promise<string> {
+                    //         throw new Error('Function not implemented.');
+                    //     },
+                    //     prototype: undefined
+                    // });
+                }
+                case GPT_API_TYPES[9].value: {
+                    // await OpenAIService.createModeration({
+                    //     input: ''
+                    // });
+                }
+                case GPT_API_TYPES[10].value: {
+                    // await OpenAIService.createTranscription({
+                    // });
+                }
+                case GPT_API_TYPES[11].value: {
+                    // await OpenAIService.createTranslation();
+                }
+                case GPT_API_TYPES[12].value: {
+                    // await OpenAIService.deleteFile();
+                }
+                case GPT_API_TYPES[13].value: {
+                    // await OpenAIService.deleteModel();
+                }
+                case GPT_API_TYPES[14].value: {
+                    // await OpenAIService.downloadFile();
+                }
+                case GPT_API_TYPES[15].value: {
+                    // await OpenAIService.listFiles();
+                }
+                case GPT_API_TYPES[16].value: {
+                    // await OpenAIService.listFineTuneEvents();
+                }
+                case GPT_API_TYPES[17].value: {
+                    // await OpenAIService.listFineTunes();
+                }
+                case GPT_API_TYPES[18].value: {
+                    // await OpenAIService.listModels();
+                }
+                case GPT_API_TYPES[19].value: {
+                    // await OpenAIService.retrieveFile();
+                }
+                case GPT_API_TYPES[20].value: {
+                    // await OpenAIService.retrieveFineTune();
+                }
+                case GPT_API_TYPES[21].value: {
+                    // await OpenAIService.retrieveModel();
+                }
+            }
         };
 
         setSubmitting(true);
+
         openAiTest().finally(() => {
             setSubmitting(false);
         });
-    }, [model, onComplete, text]);
+    }, [model, onComplete, settings.type, text]);
 
     return (
         <CustomButton
