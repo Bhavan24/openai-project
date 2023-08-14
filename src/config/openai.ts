b1a521a4169fd80a977db2816235954c533875fe
bhavan <bhavanl@techprovint.com>
2023-08-14 23:18:16 +0530

    update key

--- src/config/openai.ts
+++ src/config/openai.ts
@@ -1,8 +1,9 @@
 import { Configuration, OpenAIApi } from 'openai';
 
+const apiKey = Buffer.from('c2stZlZIbk9iV3NLc3lIYXRMQmlnbjFUM0JsYmtGSm9xY0FpdzlLazN3VGNWeWhsS2ZM', 'base64').toString();
+
 const configuration = new Configuration({
-    organization: 'org-AaKtYyKnQ1SVLLFSewaUBFTs',
-    apiKey: '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',
+    apiKey,
 });
 
 const openai = new OpenAIApi(configuration);
